76e4ef102c785f0ee0edc64c44013961b6f6bc39
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-05 13:37:57 +0200

    create variables

--- P6DataTypes/Program.cs
+++ P6DataTypes/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// mandel
 
-Console.WriteLine("Hello, World!");
+string outputString = "Hello World";
+Console.WriteLine(outputString);
+
+float decimalValue = 1f/3f;
+Console.WriteLine(decimalValue);
+
+bool isWinning = true;
+Console.WriteLine(isWinning);
+
+
+
+
+
+    
