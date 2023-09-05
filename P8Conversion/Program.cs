ec22f85bb03bd21e134b95f3ebdce047d28187e6
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-05 16:04:35 +0200

    convert to double

--- P8Conversion/Program.cs
+++ P8Conversion/Program.cs
@@ -1,2 +1,13 @@
 ﻿// Mandel
 
+using System.Globalization;
+
+Console.WriteLine("Give me a number.");
+string input = Console.ReadLine();
+
+var d = Convert.ToDouble(input);
+Console.WriteLine(input);
+
+
+
+    
