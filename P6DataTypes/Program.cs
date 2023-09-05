dfa2cbb3dafc4da4121bc9a135131875f8492109
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-05 13:57:48 +0200

    Submit P6

--- P6DataTypes/Program.cs
+++ P6DataTypes/Program.cs
@@ -3,12 +3,14 @@
 string outputString = "Hello World";
 Console.WriteLine(outputString);
 
-float decimalValue = 1f/3f;
+float decimalValue = 0.3f;
 Console.WriteLine(decimalValue);
 
 bool isWinning = true;
 Console.WriteLine(isWinning);
 
+byte hexByte = 0xa7;
+Console.WriteLine(hexByte);
 
 
 
