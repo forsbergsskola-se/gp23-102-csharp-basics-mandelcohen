73989b999535bf240b76563b6dc6b95eaff5eda5
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-06 12:01:34 +0200

    Add P9 exercises

---
+++ P9OperatorsExercises/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// exercise 1
+
+Console.WriteLine(@"Welcome! 
+Please enter a value in km/h.");
+string input = Console.ReadLine();
+
+float number = float.Parse(input);
+float x = number / 60 / 60 * 1000;
+Console.WriteLine($"That would be {x} m/s");
+
+
+// excercise 2
