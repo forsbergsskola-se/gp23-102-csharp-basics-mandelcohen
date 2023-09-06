ec8a48c56172af1e18491bfbd402048f9c4cc46c
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-06 13:59:25 +0200

    no 7

--- P9OperatorsExercises/Program.cs
+++ P9OperatorsExercises/Program.cs
@@ -2,26 +2,58 @@
 
 // exercise 1 
 
-Console.WriteLine(@"Welcome! 
-Please enter a value in km/h.");
-String userInput = Console.ReadLine();
+// Console.WriteLine(@"Welcome! 
+// Please enter a value in km/h.");
+// String userInput = Console.ReadLine();
 
-float userNumber = float.Parse(userInput);
-float x = userNumber / 60 / 60 * 1000;
-Console.WriteLine($"That would be {x} m/s");
-
-
-// exercise 2
-
-Console.WriteLine("Please enter number of minutes you want to convert to seconds.");
-userInput = Console.ReadLine();
-
-userNumber = float.Parse(userInput);
-float y = userNumber * 60;
-Console.WriteLine($"There are {y} seconds in {userInput} minutes.");    
-
-// exercise 3
+// float userNumber = float.Parse(userInput);
+// float x = userNumber / 60 / 60 * 1000;
+// Console.WriteLine($"That would be {x} m/s");
 
 
+// exercise 1.2
+
+// Console.WriteLine("Please enter number of minutes you want to convert to seconds.");
+// userInput = Console.ReadLine();
+
+// userNumber = float.Parse(userInput);
+// float y = userNumber * 60;
+// Console.WriteLine($"There are {y} seconds in {userInput} minutes.");    
+
+// exercise 2 & 3 & 6 (change + to / or * in last line)
+
+// Console.WriteLine("Enter first value:");
+// var inputA = Console.ReadLine();
+// Console.WriteLine("Enter second value:");
+// var inputB = Console.ReadLine();
+
+// int a = int.Parse(inputA);
+// int b = int.Parse(inputB);
+
+// Console.WriteLine( a / b );
+
+
+// exercise 4
+
+// Console.WriteLine("Enter radius in meters:");
+// var input = Console.ReadLine();
+// float r = float.Parse(input);
+
+// double a = r * r * 3.141592;
+// Console.WriteLine($"Area = {a} m2");
+
+// exercise 5
+
+// exercise 7
+
+Console.WriteLine("Enter your weight in kg:");
+string inputA = Console.ReadLine();
+Console.WriteLine("Enter your height in cm:");
+string inputB = Console.ReadLine();
+
+int a = int.Parse(inputA);
+int b = int.Parse(inputB);
+
+Console.WriteLine( a * b );
 
 
