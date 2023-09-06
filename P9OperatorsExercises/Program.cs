b812943d28a6c88ef5b3b23fee18dc058362003c
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-06 12:07:54 +0200

    First exercise

--- P9OperatorsExercises/Program.cs
+++ P9OperatorsExercises/Program.cs
@@ -4,11 +4,12 @@
 
 Console.WriteLine(@"Welcome! 
 Please enter a value in km/h.");
-string input = Console.ReadLine();
+String userInput = Console.ReadLine();
 
-float number = float.Parse(input);
-float x = number / 60 / 60 * 1000;
+float userNumber = float.Parse(userInput);
+float x = userNumber / 60 / 60 * 1000;
 Console.WriteLine($"That would be {x} m/s");
 
 
 // excercise 2
+
