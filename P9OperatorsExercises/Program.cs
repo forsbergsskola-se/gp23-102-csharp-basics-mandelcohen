544418985a9b72678f4afb88feb54851efc49a0a
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-06 15:33:40 +0200

    clean up exercises

--- P9OperatorsExercises/Program.cs
+++ P9OperatorsExercises/Program.cs
@@ -2,96 +2,123 @@
 
 // exercise 1 
 
-// Console.WriteLine(@"Welcome! 
-// Please enter a value in km/h.");
-// String userInput = Console.ReadLine();
+Console.WriteLine(@"Welcome! 
+Please enter a value in km/h.");
+String userInput = Console.ReadLine();
 
-// float userNumber = float.Parse(userInput);
-// float x = userNumber / 60 / 60 * 1000;
-// Console.WriteLine($"That would be {x} m/s");
+float userNumber = float.Parse(userInput);
+float x = userNumber / 60 / 60 * 1000;
+Console.WriteLine($"That would be {x} m/s");
 
 
 // exercise 1.2
 
-// Console.WriteLine("Please enter number of minutes you want to convert to seconds.");
-// userInput = Console.ReadLine();
+Console.WriteLine("Please enter number of minutes you want to convert to seconds.");
+userInput = Console.ReadLine();
 
-// userNumber = float.Parse(userInput);
-// float y = userNumber * 60;
-// Console.WriteLine($"There are {y} seconds in {userInput} minutes.");    
+userNumber = float.Parse(userInput); 
+float y = userNumber * 60;
+Console.WriteLine($"There are {y} seconds in {userInput} minutes.");    
 
-// exercise 2 & 3 & 6 (change + to / or * in last line)
+// exercise 2 
 
-// Console.WriteLine("Enter first value:");
-// var inputA = Console.ReadLine();
-// Console.WriteLine("Enter second value:");
-// var inputB = Console.ReadLine();
+Console.WriteLine("Enter first value:");
+string inputA = Console.ReadLine();
+Console.WriteLine("Enter second value:");
+string inputB = Console.ReadLine();
 
-// int a = int.Parse(inputA);
-// int b = int.Parse(inputB);
+float a = float.Parse(inputA);
+float b = float.Parse(inputB);
 
-// Console.WriteLine( a / b );
+Console.WriteLine( a / b );
+
+// exercise 3
+
+Console.WriteLine("Enter first value:");
+var inputA = Console.ReadLine();
+Console.WriteLine("Enter second value:");
+var inputB = Console.ReadLine();
+
+int a = int.Parse(inputA);
+int b = int.Parse(inputB);
+
+Console.WriteLine( a / b );
 
 
 // exercise 4
 
-// Console.WriteLine("Enter radius in meters:");
-// var input = Console.ReadLine();
-// float r = float.Parse(input);
+Console.WriteLine("Enter radius in meters:");
+var input = Console.ReadLine();
+float r = float.Parse(input);
 
-// double a = r * r * 3.141592;
-// Console.WriteLine($"Area = {a} m2");
+double a = r * r * 3.141592;
+Console.WriteLine($"Area = {a} m2");
 
-// exercise 5
 
-// Console.WriteLine("Enter number to negate:");
-// string input = Console.ReadLine();
+//exercise 5
 
-// int x = int.Parse(input);
+Console.WriteLine("Enter number to negate:");
+string input = Console.ReadLine();
 
-// Console.WriteLine($"-{input}");
+int x = int.Parse(input);
+
+Console.WriteLine($"-{input}");
+
+
+// exercise 6
+
+Console.WriteLine("Enter first value:");
+string inputA = Console.ReadLine();
+Console.WriteLine("Enter second value:");
+string inputB = Console.ReadLine();
+
+float a = float.Parse(inputA);
+float b = float.Parse(inputB);
+
+Console.WriteLine( a * b );
 
 
 // exercise 7
 
-// Console.WriteLine("Enter your weight in kg:");
-// string inputA = Console.ReadLine();
-// Console.WriteLine("Enter your height in meters:");
-// string inputB = Console.ReadLine();
+Console.WriteLine("Enter your weight in kg:");
+string inputA = Console.ReadLine();
+Console.WriteLine("Enter your height in meters:");
+string inputB = Console.ReadLine();
 
-// float a = float.Parse(inputA);
-// float b = float.Parse(inputB);
+float a = float.Parse(inputA);
+float b = float.Parse(inputB);
 
-// float BMI = a / (b * b);
-// Console.WriteLine($"Your BMI = {BMI}");
+float BMI = a / (b * b);
+Console.WriteLine($"Your BMI = {BMI}");
+
 
 // exercise 8
 
-// Console.WriteLine("Enter side A:");
-// string inputA = Console.ReadLine();
-// Console.WriteLine("Enter side B:");
-// string inputB = Console.ReadLine();
+Console.WriteLine("Enter side A:");
+string inputA = Console.ReadLine();
+Console.WriteLine("Enter side B:");
+string inputB = Console.ReadLine();
 
-// float a = float.Parse(inputA);
-// float b = float.Parse(inputB);
+float a = float.Parse(inputA);
+float b = float.Parse(inputB);
 
-// var h = (a * a) + (b * b);
-// var answer = Math.Sqrt(h);
+var h = (a * a) + (b * b);
+var answer = Math.Sqrt(h);
 
-// Console.WriteLine($"The Hypotenuse is: {answer}");
+Console.WriteLine($"The Hypotenuse is: {answer}");
+
 
 // exercise 9
 
-// Console.WriteLine("Enter number of seconds to covert to minutes:");
-// string seconds = Console.ReadLine();
+Console.WriteLine("Enter number of seconds to covert to minutes:");
+string seconds = Console.ReadLine();
 
-// int x = int.Parse(seconds);
+int x = int.Parse(seconds);
 
-// int s = (x % 60);
-// int m = (x - s) / 60;
+int s = (x % 60);
+int m = (x - s) / 60;
 
-
-// Console.WriteLine($"That'll be {m} minutes and {s} seconds");
+Console.WriteLine($"That'll be {m} minutes and {s} seconds");
     
 
 
