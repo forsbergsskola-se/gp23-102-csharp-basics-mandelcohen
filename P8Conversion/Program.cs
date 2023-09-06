8bd39f7aefd4e98bae1a5c6acab6839ca5395943
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-06 10:05:15 +0200

    Create P9

--- P8Conversion/Program.cs
+++ P8Conversion/Program.cs
@@ -4,10 +4,13 @@ using System.Globalization;
 
 Console.WriteLine("Give me a number.");
 string input = Console.ReadLine();
+
 double d = double.Parse(input);
 Console.WriteLine(d);
+
 int i = (int)d;
 Console.WriteLine(i);
+
 int o = int.Parse(input);
 Console.WriteLine(input);
 
