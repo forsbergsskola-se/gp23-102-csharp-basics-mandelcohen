e4d88a61e9bfd0a9ad5980a069ebb0193adc165e
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-07 12:03:41 +0200

    test all good

--- P9OperatorsExercises/Program.cs
+++ P9OperatorsExercises/Program.cs
@@ -2,13 +2,13 @@
 
 // exercise 1 
 
-// Console.WriteLine(@"Welcome! 
-// Please enter a value in km/h.");
-// String userInput = Console.ReadLine();
+Console.WriteLine(@"Welcome! 
+Please enter a value in km/h.");
+String userInput = Console.ReadLine();
 
-// float userNumber = float.Parse(userInput);
-// float x = userNumber / 60 / 60 * 1000;
-// Console.WriteLine($"That would be {x} m/s");
+float userNumber = float.Parse(userInput);
+float x = userNumber / 60 / 60 * 1000;
+Console.WriteLine($"That would be {x} m/s");
 
 
 // exercise 1.2
