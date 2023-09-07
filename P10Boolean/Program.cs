53ca3dea2a8138fcd59824ff89ff4a7311d79c88
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-07 14:09:38 +0200

    add P11

--- P10Boolean/Program.cs
+++ P10Boolean/Program.cs
@@ -3,11 +3,13 @@
 Console.WriteLine("What is your age: ");
 var age = int.Parse(Console.ReadLine());
 
-bool isChild = age <= 12;
-bool isTeen = age <= 19;
+bool isChild = age < 13;
+bool isTeen = age is >= 13 and >= 19;
 bool isAdult = age > 19;
 
-Console.WriteLine($"You are a child!: {isChild}");
-Console.WriteLine($"You are a teenager!: {isTeen}");
-Console.WriteLine($"You are an adult!: {isAdult}");
+Console.WriteLine(@$"You are a child!: {isChild}
+You are a teenager!: {isTeen}
+You are an adult!: {isAdult}");
+
+
 
