71b76fea28af83c7eb405d1afa5ad343932c8d6d
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-07 12:18:02 +0200

    clean up P9

--- P9Operators/Program.cs
+++ P9Operators/Program.cs
@@ -3,20 +3,16 @@
 Console.WriteLine("Give me a number of seconds: ");
 var userInput = int.Parse(Console.ReadLine());
 
-int remainingH;
-var days = Math.DivRem(userInput, 86400, out remainingH);
-int remainingM;
-var hours = Math.DivRem(remainingH, 3600, out remainingM);
-int remainingS;
-var minutes = Math.DivRem(remainingM, 60, out remainingS);
+var days = Math.DivRem(userInput, 86400, out int remainingH);
+var hours = Math.DivRem(remainingH, 3600, out int remainingM);
+var minutes = Math.DivRem(remainingM, 60, out int remainingS);
 
 Console.WriteLine($"Seconds: {remainingS}");
 Console.WriteLine($"Minutes: {minutes}");
 Console.WriteLine($"Hours: {hours}");
 Console.WriteLine($"Days: {days}");
 
-TimeSpan ts = new TimeSpan(1, 10, 17, 36);
-Console.WriteLine(ts.ToString(@"d\.hh\:mm\:ss"));
+Console.WriteLine($"{days}:{hours}:{minutes}:{remainingS}");
 
 var total = Convert.ToDecimal(userInput) / 86400;
 Console.WriteLine($"In total, that's {total}");
