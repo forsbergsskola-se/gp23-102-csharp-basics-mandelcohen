ffe904267920a0aef6dcda8098aedd0bac69ba77
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-07 14:27:32 +0200

    If else age

--- P11IfElse/Program.cs
+++ P11IfElse/Program.cs
@@ -1,2 +1,19 @@
 ﻿// mandel
 
+Console.WriteLine("What is your age: ");
+var age = int.Parse(Console.ReadLine());
+
+if (age < 13) {
+    Console.WriteLine("You are a child.");
+}
+else if (age is >= 13 and <= 19) {
+    Console.WriteLine("You are a teenager.");
+}
+else {
+    Console.WriteLine("You are an adult.");
+}
+
+Console.WriteLine("Type another integer: ");
+var number = int.Parse(Console.ReadLine());
+
+
