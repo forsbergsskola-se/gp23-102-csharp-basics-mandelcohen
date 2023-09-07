dc4d9f795a49efa12089a4cc0657989f80148f72
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-07 16:49:47 +0200

    P12 random works

--- P12Random/Program.cs
+++ P12Random/Program.cs
@@ -1,13 +1,37 @@
 ﻿// mandel
 
+using System.Numerics;
 using System.Security.Cryptography;
 
-Console.WriteLine("Please pass me a sees (integer)");
+Console.WriteLine("Please pass me a seed (integer)");
 var seed = int.Parse(Console.ReadLine());
 
 var random = new Random(seed);
-var number = random.Next(0, 5);
+Console.WriteLine(random.Next(0, maxValue: 5));
+Console.WriteLine(random.Next(0, maxValue: 5));
+Console.WriteLine(random.Next(0, maxValue: 5));
 
+Console.WriteLine("Three numbers between 0.0 and 0.5:");
 
+Console.WriteLine(random.NextDouble()*0.5);
+Console.WriteLine(random.NextDouble()*0.5);
+Console.WriteLine(random.NextDouble()*0.5);
 
+Console.WriteLine("Three numbers between 0.2 and 0.7:");
+
+Console.WriteLine(random.NextDouble()*(0.7 - 0.2)+0.2);
+Console.WriteLine(random.NextDouble()*(0.7 - 0.2)+0.2);
+Console.WriteLine(random.NextDouble()*(0.7 - 0.2)+0.2);
+
+Console.WriteLine("Give me a crit chance between 0.0 (0%) and 1.0 (100%)");
+var chance = double.Parse(Console.ReadLine());
+
+if (Random.Shared.NextDouble() < chance)
+{
+    Console.WriteLine("Crit");
+}
+else
+{
+    Console.WriteLine("No Crit");
+}
 
