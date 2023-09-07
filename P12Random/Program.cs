2a2e7042b7744e73fe6aea35874e19837d30789f
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-07 15:45:39 +0200

    add P12

---
+++ P12Random/Program.cs
@@ -0,0 +1,13 @@
+﻿// mandel
+
+using System.Security.Cryptography;
+
+Console.WriteLine("Please pass me a sees (integer)");
+var seed = int.Parse(Console.ReadLine());
+
+var random = new Random(seed);
+var number = random.Next(0, 5);
+
+
+
+
