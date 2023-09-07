e68b50113a3335a4cfdaeaef0b37b0c9a348bb6f
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-07 10:51:40 +0200

    P9

--- P9Operators/Program.cs
+++ P9Operators/Program.cs
@@ -1,4 +1,27 @@
 ﻿// mandel
 
+Console.WriteLine("Give me a number of seconds: ");
+var userInput = int.Parse(Console.ReadLine());
+
+int remainingSeconds;
+var days = Math.DivRem(userInput, 86400, out remainingSeconds);
+
+int remainingHours;
+var hours = Math.DivRem(remainingSeconds, 3600, out remainingHours);
+
+int remainingMinutes;
+var minutes = Math.DivRem(remainingHours, 60, out remainingMinutes);
+
+var seconds = Math.DivRem(remainingMinutes, 60 out )
 
 
+
+
+
+
+
+Console.WriteLine("Give me a number of seconds: ");
+Console.WriteLine("Give me a number of seconds: ");
+
+
+//$ Console.WriteLine($@"Seconds: {}"); Console.WriteLine($@"Minutes: {minutes}"); Console.WriteLine($@"Hours: {hours}"); Console.WriteLine($@"Days: {days}");
