764768c40b914d1ccea63a6ff44608dc529204a2
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-08 11:24:35 +0200

    P13 work

--- P13GoTo/Program.cs
+++ P13GoTo/Program.cs
@@ -1,5 +1,22 @@
 ﻿// mandel
 
-Console.WriteLine("I have picked a number (0-100) It's your turn to guess it!");
-var number = int.Parse(Console.ReadLine());
+using System.ComponentModel.Design;
 
+var random = new Random();
+
+Console.WriteLine("I have picked a number (0-100) It's your turn to guess it!");
+var UserNumber = int.Parse(Console.ReadLine());
+
+userTurn:
+if (random > UserNumber) {
+    Console.WriteLine("Nope! My number is greater.");
+    goto userTurn;
+}
+else if (random < UserNumber) {
+    Console.WriteLine("Nope! My number is smaller.");
+    goto userTurn;
+}
+
+else (random = UserNumber){
+    Console.WriteLine("That's the number! Well played.");
+}
