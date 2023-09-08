39eb661b77c449358826a13be1f46948c926d4be
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-08 13:00:04 +0200

    add counter P13

--- P13GoTo/Program.cs
+++ P13GoTo/Program.cs
@@ -4,21 +4,39 @@ gameStart: ;
 var myNumber = Random.Shared.Next(0, maxValue:100);
 Console.WriteLine("I have picked a number (0-100) It's your turn to guess it!");
 
-userTurn: ;
-var userNumber = int.Parse(Console.ReadLine());  
+var counter = 10; 
 
-if (myNumber > userNumber) {
-    Console.WriteLine("Nope! My number is greater.");
+userTurn: ;
+var userNumber = int.Parse(Console.ReadLine());
+
+if (myNumber > userNumber && (--counter > 0)) {
+    Console.WriteLine($@"Nope! My number is greater. 
+You have {counter} tries left, guess again!");
     goto userTurn;
 }
-else if (myNumber < userNumber) {
-    Console.WriteLine("Nope! My number is smaller.");
+else if (myNumber < userNumber && (--counter > 0)) {
+    Console.WriteLine($@"Nope! My number is smaller.
+You have {counter} tries left, guess again!");
     goto userTurn;
 }
-else {
-    Console.WriteLine(@"That's the number! Well played.
+else if (myNumber == userNumber) {
+    Console.WriteLine(@"That's the number! Well played.             
+                                                                    
+Go again!                                                           
+");                                                                 
+    goto gameStart;                                                 
+}
+else if (myNumber != userNumber || (counter == 0)) {
+       Console.WriteLine(@"You are out of tries :(
 
 Go again!
 ");
-    goto gameStart;
-}
+       goto gameStart;
+}    
+
+
+
+
+
+
+
