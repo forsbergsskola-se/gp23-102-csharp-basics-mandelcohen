8b4a12616059fdbbb628151aff58cc15bc28cbcd
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-08 14:19:27 +0200

    Add ex2

--- P14Extra/Program.cs
+++ P14Extra/Program.cs
@@ -1,10 +1,23 @@
 ﻿// mandel
 
 // Toggle 0/1
-
+/*
 goStart: ;
 Console.WriteLine($"Enter 1 or 0");
 var userInput = int.Parse(Console.ReadLine());
 
 Console.WriteLine(1 - userInput);
 goto goStart;
+*/
+ 
+// Maximum
+
+goStart: ;
+Console.WriteLine($"Enter first value)");
+var userInput1 = int.Parse(Console.ReadLine());
+Console.WriteLine($"Enter second value)");
+var userInput2 = int.Parse(Console.ReadLine());
+
+int max = userInput1 > userInput2 ? userInput1 : userInput2;
+
+Console.WriteLine($"The maximum is {max}.");
