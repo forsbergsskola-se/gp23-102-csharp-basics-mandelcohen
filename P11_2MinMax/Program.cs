98fd64244b33fc47acc55273ff3384d12fbea6c2
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-11 11:45:30 +0200

    P11_2 update

--- P11_2MinMax/Program.cs
+++ P11_2MinMax/Program.cs
@@ -1,3 +1,8 @@
 ﻿// mandel
 
-Console.WriteLine("Input three numbers: (eg 12,-7,100");
+Console.WriteLine("Input first number");
+var inputA = int.Parse(Console.ReadLine());
+Console.WriteLine("Input second number");
+var inputB = int.Parse(Console.ReadLine());
+Console.WriteLine("Input third number");
+var inputC = int.Parse(Console.ReadLine());
