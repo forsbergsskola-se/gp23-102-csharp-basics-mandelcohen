ee6cac663b144b6faba4bc5496ea2e9431ea6e08
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-11 14:06:53 +0200

    add P11_4Calculator

--- P11_3Characters/Program.cs
+++ P11_3Characters/Program.cs
@@ -1,4 +1,7 @@
 ﻿// mandel
 
 Console.WriteLine("Type a single character:");
+var userInput = int.Parse(Console.ReadLine();
+
+
 
---
+++ P11_4Calulator/Program.cs
@@ -0,0 +1,8 @@
+﻿// mandel
+
+Console.WriteLine("Enter first value:");
+var inputA = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the operand:");
+var operand 
+Console.WriteLine("Enter the second value:");
+var inputB = int.Parse(Console.ReadLine());
