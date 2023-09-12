57bdb4e70b0597455aea37902a5cc413a4cfbddd
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-12 13:04:50 +0200

    Submit P11_5EvenOdd

--- P11_5EvenOdd/Program.cs
+++ P11_5EvenOdd/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Give me an integer:");
+int userInput = int.Parse(Console.ReadLine());
+
+Console.WriteLine(userInput % 2 == 0 ? "Your number is even." : "Your number is odd.");
