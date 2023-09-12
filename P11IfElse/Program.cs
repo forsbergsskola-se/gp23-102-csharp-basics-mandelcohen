1d1923efd8183636f752149e1a7fff4fe56e4b6b
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-12 09:44:48 +0200

    Update P11

--- P11IfElse/Program.cs
+++ P11IfElse/Program.cs
@@ -3,12 +3,11 @@
 Console.WriteLine("What is your age: ");
 var age = int.Parse(Console.ReadLine());
 
-switch (age)
-{
+switch (age) {
     case < 13:
         Console.WriteLine("You are a child.");
         break;
-    case >= 13 and <= 19:
+    case <= 19:
         Console.WriteLine("You are a teenager.");
         break;
     default:
@@ -22,9 +21,4 @@ var number = int.Parse(Console.ReadLine());
 var max = age > number ? age : number;
 Console.WriteLine($"The maximum is: {max}");
 
-if (max%2 == 0) {
-    Console.WriteLine("Your number is even.");
-}
-else {
-    Console.WriteLine("Your number is odd.");
-}
+Console.WriteLine(max % 2 == 0 ? "Your number is even." : "Your number is odd.");
