a223e3c86ce836360ec6a7d06ee83d4ae4ecd30d
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-14 12:13:27 +0200

    Update P9

--- P9Operators/Program.cs
+++ P9Operators/Program.cs
@@ -1,21 +1,29 @@
 ﻿// mandel
 
-
 Console.WriteLine("Give me a number of seconds: ");
-var userInput = int.Parse(Console.ReadLine());
+int userInput = int.Parse(Console.ReadLine());
 
-var days = Math.DivRem(userInput, 86400, out int remainingH);
-var hours = Math.DivRem(remainingH, 3600, out int remainingM);
-var minutes = Math.DivRem(remainingM, 60, out int remainingS);
+/*
+int days = userInput / 86400;
+int remainingH = userInput % 86400;
+int hours = remainingH / 3600;
+int remainingM = remainingH % 3600;
+int min = remainingM / 60;
+int remainingS = remainingM % 60;
+*/
+
+int days = Math.DivRem(userInput, 86400, out int remainingH);
+int hours = Math.DivRem(remainingH, 3600, out int remainingM);
+int min = Math.DivRem(remainingM, 60, out int remainingS);
 
 Console.WriteLine($"Seconds: {remainingS}");
-Console.WriteLine($"Minutes: {minutes}");
+Console.WriteLine($"Minutes: {min}");
 Console.WriteLine($"Hours: {hours}");
 Console.WriteLine($"Days: {days}");
 
-Console.WriteLine($"{days}.{hours}:{minutes}:{remainingS}");
+Console.WriteLine($"{days}.{hours}:{min}:{remainingS}");
 
-var total = Convert.ToDecimal(userInput) / 86400;
+decimal total = Convert.ToDecimal(userInput) / 86400;
 Console.WriteLine($"In total, that's {total}");
 
 
