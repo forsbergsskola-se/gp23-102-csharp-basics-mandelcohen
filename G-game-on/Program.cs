63e91877d9f36a75f6de9d41134104b445d5a51e
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-14 10:53:51 +0200

    Add toggle to NIM

--- G-game-on/Program.cs
+++ G-game-on/Program.cs
@@ -2,48 +2,49 @@
 
 Console.WriteLine("Welcome to NIM!");
 
-// playerTurn, toggle player and ai until one win or lose
-
-
 gameStart: ;
 Console.WriteLine("|||||||||||||||||||||||| (24)");  
 int matches = 24;
+bool playerTurn = true;
 
-playerTurn: ;
-Console.WriteLine("How many matches do you want to draw?");
-int userDraw = int.Parse(Console.ReadLine());
+drawPhase: ;
 
-if (userDraw is > 0 and > 3) {
-    Console.Write("You can only draw 1,2 or 3 matches at time! ");
-    goto playerTurn;
-} 
-matches -= userDraw;
-
-if (matches <= 0) {
-    Console.WriteLine("You lose!");
-    goto playerTurn;
+if (playerTurn){
+    Console.WriteLine("How many matches do you want to draw?");
+    int userDraw = int.Parse(Console.ReadLine());
+    
+    if (userDraw is > 3 or < 1) {
+        Console.Write("You can only draw 1,2 or 3 matches at time! ");
+        goto drawPhase;
+    } 
+    matches -= userDraw;
+}
+if (!playerTurn) {
+    Random random = new Random();
+    int aiTurn = random.Next(1, 4);
+    
+    matches -= aiTurn;
+    Console.WriteLine($"The AI draws {aiTurn}");
+    
+    int matchPrint = matches;
+    printMatches: ;
+    Console.Write('|');
+    if (matchPrint > 1) { 
+        matchPrint--; 
+        goto printMatches;
+    }
+    Console.WriteLine($"({matches})");
 }
 
-Random random = new Random();
-int aiTurn = random.Next(1, 4);
-
-Console.WriteLine($"The AI draws {aiTurn}");
-matches -= aiTurn;
-
-if (matches <= 0) {
-    Console.WriteLine("You won!");
+if (matches < 1 && playerTurn) {
+    Console.WriteLine("You lose!");
+    goto gameStart;
+}
+if (matches < 1 && !playerTurn){
+    Console.WriteLine("You win!");
     goto gameStart;
 }
 
-int matchPrint = matches;
-printMatches: ;
-Console.Write('|');
-if (matchPrint > 1)
-{
-    matchPrint--;
-    goto printMatches;
-}
-Console.WriteLine($"({matches})");
-
-goto playerTurn ;
+playerTurn = !playerTurn;
+goto drawPhase ;
 
