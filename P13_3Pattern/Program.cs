748cd37694cd95870ab28b9a98a74fa6b5c25e73
mandel cohen <mandel.cohen@student.forsbergsskola.se>
2023-09-15 11:02:37 +0200

    Attempt for loop P13_3

--- P13_3Pattern/Program.cs
+++ P13_3Pattern/Program.cs
@@ -1,5 +1,11 @@
 ﻿// mandel
 
 Console.WriteLine("What size do you want your pattern to be?");
-int userImput = int.Parse(Console.ReadLine());
+int userInput = int.Parse(Console.ReadLine());
 
+for (int rows = userInput ; rows <= userInput; rows--) {
+/*   for (int draw = userInput; draw >= rows && rows > 0 ; draw++) {
+       Console.Write("#-");    
+}
+    Console.WriteLine();
+} */
